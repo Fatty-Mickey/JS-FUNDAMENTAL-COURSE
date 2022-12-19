ce644813473e48001ed350a34c35274b711d3c9f
Maksym Bilyk <109813957+Fatty-Mickey@users.noreply.github.com>
2022-12-19 14:23:33 +0200

    homework7

--- HOMEWORK7/index.js
+++ HOMEWORK7/index.js
@@ -1,5 +1,5 @@
 // ************************ Task 1 ************************
-// let openTab = window.open("", "w3schools", "width=300");
+// let openTab = window.open("", "w3schools", "width=300, height = 300");
 
 // setTimeout(function () {
 //     openTab.resizeTo(500,500)
@@ -12,7 +12,13 @@
 // setTimeout(() => openTab.close(), 6000);
 
 // ************************ Task 2 ************************
+// document.body.innerHTML = `
+//     <p id='text'>I learning JavaScript events!</p>
+//     <div>
+//         <button id="button-0">Change style!</button>
+//     </div>`;
 // const paragraph = document.querySelector('#text');
+// const btn0 = document.querySelector('#button-0')
 
 // function changeCSS() {
 //     paragraph.style.color = 'orange';
@@ -20,9 +26,16 @@
 //     paragraph.style.fontFamily = "Comic Sans MS";
 // };
 
-// paragraph.addEventListener('click', changeCSS);
+// btn0.addEventListener('click', changeCSS);
 
 // ************************ Task 3 ************************
+// document.body.innerHTML = `
+//         <div class="">
+//             <button id="button-1" type="button">Button 1</button>
+//             <button id="button-2" type="button">Button 2</button>
+//             <button id="button-3" type="button">Button 3</button>
+//             <a id="link" href="">link</a>
+//         </div>`;
 // const btn1 = document.querySelector('#button-1');
 // const btn2 = document.querySelector('#button-2');
 // const btn3 = document.querySelector('#button-3');
@@ -42,14 +55,25 @@
 // document.querySelector('#link').addEventListener('mouseout', () => document.body.style.backgroundColor = 'white');
 
 // ************************ Task 4 ************************
+// document.body.innerHTML = `
+//         <select name="user_profile_color_1">
+//             <option value="blue">blue</option>
+//             <option value="green">green</option>
+//             <option value="yellow">yellow</option>
+//             <option value="red">red</option>
+//             <option value="orange">orange</option>
+//             <option value="black">black</option>
+//         </select>
+//         <button id="button-4" type="button">Choose and delete</button>`;
+
 // const btn4 = document.querySelector('#button-4');
 // const select = document.querySelector('select');
-
-// btn4.addEventListener('click', function () {
-//     select.options[select.options.selectedIndex].remove();
-// });
+// btn4.addEventListener('click', () => select.options[select.options.selectedIndex].remove());
 
 // ************************ Task 5 ************************
+// document.body.innerHTML = `
+//         <button id="button-5" type="button">Live button</button>
+//         <div></div>`;
 // const btn5 = document.querySelector('#button-5');
 // const div = document.querySelector('div');
 
@@ -58,50 +82,51 @@
 // btn5.addEventListener('mouseout', () => div.innerHTML = `<p>Mouse is not on me!</p>`);
 
 // ************************ Task 6 ************************
+// document.body.innerHTML = `<div></div>`;
 // const div = document.querySelector('div');
 // window.addEventListener('resize', () => div.innerHTML = `Window height: ${window.innerHeight}; Window width: ${window.innerWidth}`);
 
 // ************************ Task 7 ************************
-document.body.innerHTML = `
-       <select name="country" id="country">
-            <option value="ger">Germany</option>
-            <option value="usa">USA</option>
-            <option value="ukr">Ukraine</option>
-        </select>
+// document.body.innerHTML = `
+//        <select name="country" id="country">
+//             <option value="ger">Germany</option>
+//             <option value="usa">USA</option>
+//             <option value="ukr">Ukraine</option>
+//         </select>
         
-        <select name="cities" id="cities">
-            <option value="berlin">Berlin</option>
-            <option value="hamburg">Hamburg</option>
-            <option value="munich">Munich</option>
-        </select>
-        <p>Germany, Berlin</p>`;
-const selectCountry = document.querySelector('#country');
-const selectCity = document.querySelector('#cities');
+//         <select name="cities" id="cities">
+//             <option value="berlin">Berlin</option>
+//             <option value="hamburg">Hamburg</option>
+//             <option value="munich">Munich</option>
+//         </select>
+//         <p>Germany, Berlin</p>`;
+// const selectCountry = document.querySelector('#country');
+// const selectCity = document.querySelector('#cities');
 
-selectCountry.addEventListener('change', () => {
-    if (selectCountry.options[selectCountry.options.selectedIndex].value === 'ger') {
-        selectCity.innerHTML = `
-            <option value="berlin">Berlin</option>
-            <option value="hamburg">Hamburg</option>
-            <option value="munich">Munich</option>`
-    } else if (selectCountry.options[selectCountry.options.selectedIndex].value === 'usa') {
-        selectCity.innerHTML = `
-            <option value="washington">Washington</option>
-            <option value="losAngeles">Los Angeles</option>
-            <option value="chicago">Chicago</option>`
-    } else if (selectCountry.options[selectCountry.options.selectedIndex].value === 'ukr') {
-        selectCity.innerHTML = `
-            <option value="kyiv">Kyiv</option>
-            <option value="kharkiv ">Kharkiv </option>
-            <option value="odesa">Odesa</option>`
-    }
-    const selectedCountry = selectCountry.options[selectCountry.options.selectedIndex].innerHTML;
-    const selectedCity = selectCity.options[selectCity.options.selectedIndex].innerHTML;
-    document.querySelector('p').innerHTML = `${selectedCountry}, ${selectedCity}`;
-});
+// selectCountry.addEventListener('change', () => {
+//     if (selectCountry.options[selectCountry.options.selectedIndex].value === 'ger') {
+//         selectCity.innerHTML = `
+//             <option value="berlin">Berlin</option>
+//             <option value="hamburg">Hamburg</option>
+//             <option value="munich">Munich</option>`
+//     } else if (selectCountry.options[selectCountry.options.selectedIndex].value === 'usa') {
+//         selectCity.innerHTML = `
+//             <option value="washington">Washington</option>
+//             <option value="losAngeles">Los Angeles</option>
+//             <option value="chicago">Chicago</option>`
+//     } else if (selectCountry.options[selectCountry.options.selectedIndex].value === 'ukr') {
+//         selectCity.innerHTML = `
+//             <option value="kyiv">Kyiv</option>
+//             <option value="kharkiv ">Kharkiv </option>
+//             <option value="odesa">Odesa</option>`
+//     }
+//     const selectedCountry = selectCountry.options[selectCountry.options.selectedIndex].innerHTML;
+//     const selectedCity = selectCity.options[selectCity.options.selectedIndex].innerHTML;
+//     document.querySelector('p').innerHTML = `${selectedCountry}, ${selectedCity}`;
+// });
 
-selectCity.addEventListener('change', function () {
-    const selectedCountry = selectCountry.options[selectCountry.options.selectedIndex].innerHTML;
-    const selectedCity = selectCity.options[selectCity.options.selectedIndex].innerHTML;
-    document.querySelector('p').innerHTML = `${selectedCountry}, ${selectedCity}`;
-});
+// selectCity.addEventListener('change', function () {
+//     const selectedCountry = selectCountry.options[selectCountry.options.selectedIndex].innerHTML;
+//     const selectedCity = selectCity.options[selectCity.options.selectedIndex].innerHTML;
+//     document.querySelector('p').innerHTML = `${selectedCountry}, ${selectedCity}`;
+// });
