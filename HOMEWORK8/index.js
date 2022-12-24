5adc26cd4c2a29cdfb20528e941f3faf914d335b
Maksym Bilyk <109813957+Fatty-Mickey@users.noreply.github.com>
2022-12-24 18:19:35 +0200

    homework8

--- HOMEWORK8/index.js
+++ HOMEWORK8/index.js
@@ -1,14 +1,15 @@
 // ************************ Task 1 ************************
 const upperCase = str => /^[A-Z]/.test(str) ? "String's starts with uppercase character" : "String's not starts with uppercase character";
-console.log(upperCase('Aaa'));
+console.log(upperCase('RegExp'));
+console.log(upperCase('regExp'));
 
 // ************************ Task 2 ************************
-const checkEmail = str => /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu.test(str);
-console.log(checkEmail("Qmail2@gmail.com"));
+// const checkEmail = str => /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu.test(str);
+// console.log(checkEmail("Qmail2@gmail.com"));
 
 // ************************ Task 3 ************************
-const re3 = /db*d/gi;
-console.log('cdbBdbsbz'.match(re3));
+const re3 = /d(b+)(d)/gi;
+console.log(re3.exec('cdbBdbsbz'));
 
 // ************************ Task 4 ************************
 function rev(str) {
@@ -22,14 +23,19 @@ const cardVal = str5 => /^(\d{4}\-){3}(\d{4})$/.test(str5);
 console.log(cardVal('9999-9999-9999-9999'));
 
 // ************************ Task 6 ************************
-const checkEmail2 = str => /^[a-z0-9]/i.test(str) ? "Email is correct!" : "Email is not correct!";
+const checkEmail2 = str => /(^[a-z0-9]+)([a-z0-9]*\_?\-{0,1})/ig.test(str) ? "Email is correct!" : "Email is not correct!";
 console.log(checkEmail2('my_mail@gmail.com'));
 console.log(checkEmail2('#my_mail@gmail.com'));
 console.log(checkEmail2('my_ma--il@gmail.com'));
 
 // ************************ Task 7 ************************
 const checkLogin = str => {
-    console.log(/^[a-z]{1}[a-z0-9]{2,12}[^\W]/gi.test(str));
+    console.log(/^[a-z]+[0-9a-z\.]{1,12}$/gi.test(str));
+    console.log(str.match(/\d(\.\d)?/g));
+
 };
 checkLogin('ee1.1ret3');
 checkLogin('ee1*1ret3');
+
+
+
